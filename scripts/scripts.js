8206a5e454441558ce646ef8e5ed905747541334
gabrielhoppus <luizgabriel.84@hotmail.com>
2022-10-28 14:53:02 -0300

    Chat + User Connection V3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,16 @@
 let messages = [];
+let tempArray = [];
+let userName = "";
 const messageList = document.querySelector('.main_body');
 
-function userName(){
-    const name = prompt("Qual é seu nome de usuário?");
+function registerName(){
+    userName = prompt("Qual é seu nome de usuário?");
     const data ={
-        name: name
+        name: userName
     };
     const promisseName = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', data);
     promisseName.then(getMessage);
-    promisseName.catch(userName)
+    promisseName.catch(registerName)
 
     setInterval(() => {
         const promisseName = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', data);
@@ -21,7 +23,7 @@ function getMessage(){
     promisseMessage.catch(promisseError)
     setInterval(() => {
         const promisseMessage = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-        promisseMessage.then(promisseOK);
+        promisseMessage.then(checkNew);
         promisseMessage.catch(promisseError)
     }, 3000);
 }
@@ -33,11 +35,11 @@ function promisseError(){
 function promisseOK(response){
     messages = response.data;
     renderMessages();
+    scrollLast();
 }
 
 function renderMessages(){
     messageList.innerHTML = '';
-
     for (let i = 0; i < messages.length; i++){
         let message = messages[i];
         
@@ -64,21 +66,44 @@ function renderMessages(){
             </div>
             `
         }else if(message.type === "private_message"){
-            messageList.innerHTML += `
-            <div class="private_message">
-                <p class="message">
-                    <span class="time">(${message.time})</span>
-                    <strong class="from">${message.from}</strong>
-                    <span>reservadamente para</span>
-                    <strong class="to">${message.to}:</strong>
-                    <span class="text">${message.text}</span>   
-                </p>             
-            </div>
+            if (message.to === userName){
+                messageList.innerHTML += 
+                `<div class="private_message">
+                    <p class="message">
+                        <span class="time">(${message.time})</span>
+                        <strong class="from">${message.from}</strong>
+                        <span>reservadamente para</span>
+                        <strong class="to">${message.to}:</strong>
+                        <span class="text">${message.text}</span>   
+                    </p>             
+                </div>
             `
+            }
+            }
+            
         }
     }
+
+
+function checkNew(response){
+    tempArray = response.data
+    if (arrayEquals(messages, tempArray) == true){
+        return;
+    }else{
+        messages = tempArray
+        renderMessages()
+        scrollLast()
+    }
+}
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
 }
 
 function scrollLast(){
     messageList.lastElementChild.scrollIntoView()
+
 }
